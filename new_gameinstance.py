c9bbef12567a73239765923ef60358a799f3bbf7
R <R@DESKTOP-5DUICBU>
2017-11-14 18:51:46 +0100

    Georg Bush did 9/11

--- new_gameinstance.py
+++ new_gameinstance.py
@@ -6,16 +6,16 @@ def game_inst():
 	print("Do you wish to configure your own dice(s). (y/n)")
 	inp = input()
 	if (inp == "y"):
-		print("Great. How many dices? 1-99 is possible")
+		print("Great. How many dices? 1-99 are possible")
 		inp = input()
 		while (True):
 			try:
 				inp = int(inp)
-				
-				if (1 <= inp or inp <= 99):
-					break
-				print("jop")
+				break
 			except (ValueError, IndexError):
 				print("Unreadable")
+		if (1 <= inp or 99 < inp):
+			print("Dice number out of bound")
+			
 	elif(inp == "x"):
 		print("Ok. 1 dice, 6 faces.")
