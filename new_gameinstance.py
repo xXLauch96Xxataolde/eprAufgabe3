37eb7c19a6e2bb72a54836ed809d6f614505edf1
R <R@DESKTOP-5DUICBU>
2017-11-14 18:37:29 +0100

    youmustenteracommitmessage

--- new_gameinstance.py
+++ new_gameinstance.py
@@ -1,5 +1,21 @@
 import main
 from player import Player
 
+
 def game_inst():
-	print("asd")
+	print("Do you wish to configure your own dice(s). (y/n)")
+	inp = input()
+	if (inp == "y"):
+		print("Great. How many dices? 1-99 is possible")
+		inp = input()
+		while (True):
+			try:
+				inp = int(inp)
+				
+				if (1 <= inp or inp <= 99):
+					break
+				print("jop")
+			except (ValueError, IndexError):
+				print("Unreadable")
+	elif(inp == "x"):
+		print("Ok. 1 dice, 6 faces.")
