7670ea7427792327e926165fdd60535de19289ed
R <R@DESKTOP-5DUICBU>
2017-11-14 23:32:02 +0100

    asd

--- new_gameinstance.py
+++ new_gameinstance.py
@@ -5,17 +5,19 @@ from player import Player
 def game_inst():
 	print("Do you wish to configure your own dice(s). (y/n)")
 	inp = input()
-	if (inp == "y"):
-		print("Great. How many dices? 1-99 are possible")
-		inp = input()
+	if (inp == "y"):		
 		while (True):
+			print("Great. How many dices? 1-99 are possible")
+			inp = input()
 			try:
 				inp = int(inp)
-				break
+				if (0 < inp and inp < 100):
+					"Great"
+					break
+				else:
+					print("Too many dices. Repeat")
 			except (ValueError, IndexError):
 				print("Unreadable")
-		if (1 <= inp or 99 < inp):
-			print("Dice number out of bound")
-			
+
 	elif(inp == "x"):
 		print("Ok. 1 dice, 6 faces.")
